510ba40f1465543a8c841617a0511d51b118e9fc
Dmytro Honcharenko <dimeeentor@gmail.com>
2024-03-22 08:44:11 +0100

    added couple of tests

--- camlbrick_launcher.ml
+++ camlbrick_launcher.ml
@@ -1,7 +1,6 @@
 open Camlbrick;;
 open Camlbrick_gui;;
 
-
 let game = make_camlbrick();;
 let param = param_get(game);;
 
@@ -9,7 +8,6 @@ let param = param_get(game);;
   Chargez votre niveau par defaut ici   
   
 *)
-
 
 (* fonction qui lance le jeu *)
 launch_camlbrick(param,game);;
